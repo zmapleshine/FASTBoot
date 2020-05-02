1745260f90ccd1a8d8e00e1f26602c400e2b4425
zmapleshine <715017@163.com>
2020-05-02 12:16:57 +0800

    add infrastructure modules

--- fastboot-test/src/main/java/com/github/fastboot/test/Application.java
+++ fastboot-test/src/main/java/com/github/fastboot/test/Application.java
@@ -1,4 +1,4 @@
-package com.zmapleshine.fastboot.test;
+package com.github.fastboot.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
